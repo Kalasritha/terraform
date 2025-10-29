d9fd253fa5206ccc7785b9f5c61789b3ee5447e6
Kalasritha <srithu0004@example.com>
2025-10-29 14:52:53 +0000

    file changed

--- eks-cluster/eks.tf
+++ eks-cluster/eks.tf
@@ -41,3 +41,34 @@ resource "aws_eks_cluster" "eks-cluster" {
   }
 
 }
+
+
+
+resource "aws_iam_role" "cluster" {
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.cluster.name
+}
